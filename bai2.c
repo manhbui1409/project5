e0a17c74d1523097eefba1fcb8901b8e564e30a5
manhbui1409 <133564656+manhbui1409@users.noreply.github.com>
2023-09-08 20:55:23 +0700

    manh

---
+++ bai2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	int a,b,c,d;
+	a=5;
+	b=6;
+	b=7;
+	d = 10*a*a +5*b*c+c*c;
+	printf("gia tri d = %d",d);
+	
+}
---
+++ bai3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf ("===================================\n");
+	printf ("| STT | Ho Ten            | Tuoi     |  Gioi Tinh        | \n");
+	printf ("|01     | Nguyen A        | 20       | Nam               |\n");
+	printf ("|02     | Nguyen B        | 22       | Nam               |\n");
+	printf ("|03     | Nguyen C        | 26       | Nam               |\n");
+
+	 }
